6e318fec2dafe8f4586e428d48398983740fa08a
ASong5 <songandrewbms@gmail.com>
2023-08-27 21:19:54 -0400

    added binary tree fractal and plant fractal visualizations - need to figure out how to scale the branches so that the tree doesnt go past the bounds of the canvas

--- index.js
+++ index.js
@@ -1,58 +1,117 @@
 class LSystem {
-    constructor(axiom, productions){
-      this.axiom = axiom;
-      this.productions = productions;
-    }
+  grammar = {
+    fractalBTree: {
+      id: 0,
+      axiom: "0",
+      productions: {
+        0: "1[0]0",
+        1: "11",
+      },
+      //   instructions: {
+      //     "0":
+      //   }
+    },
+    fractalPlant: {
+      id: 1,
+      axiom: "X",
+      productions: {
+        X: "F+[[X]-X]-F[-FX]+X",
+        F: "FF",
+      },
+    },
+    algae: undefined,
+    sierpinski: undefined,
   };
-  
-  const grammarToString = (grammar, depth) => {
-    const generate = (axiom, productions, currDepth) => {
-      if(currDepth === 0) return axiom;
+
+  grammarToString = (grammar, depth) => {
+    const generate = (axiom, productions, currDepth = 0) => {
+      if (currDepth === 0) return axiom;
       let newAxiom = "";
-      for(char of axiom){
-        if(productions[char]){
+      for (const char of axiom) {
+        if (productions[char]) {
           newAxiom += productions[char];
-        }
-        else newAxiom += char;
+        } else newAxiom += char;
       }
       return generate(newAxiom, productions, currDepth - 1);
-    }
+    };
     return generate(grammar.axiom, grammar.productions, depth);
+  };
+
+  generateFractal(ctx, id, sentence, branchSize) {
+    if (id === 0) {
+      ctx.translate(canvas.width / 2, canvas.height);
+      ctx.shadowColor = "teal";
+      ctx.shadowBlur = 14;
+
+      let idx = 0;
+      while (idx < sentence.length) {
+        const symbol = sentence[idx];
+
+        if (symbol === "1" || symbol === "0") {
+          ctx.beginPath();
+          ctx.moveTo(0, 0);
+          ctx.lineTo(0, -branchSize);
+          ctx.stroke();
+          ctx.translate(0, -branchSize);
+          ctx.closePath();
+        } else if (symbol === "[") {
+          ctx.save();
+          ctx.rotate(-Math.PI / 4);
+        } else if (symbol === "]") {
+          ctx.restore();
+          ctx.rotate(Math.PI / 4);
+        }
+
+        idx++;
+      }
+    } else if (id === 1) {
+      ctx.translate(canvas.width / 2, canvas.height);
+      ctx.shadowColor = "teal";
+      ctx.shadowBlur = 14;
+
+      ctx.rotate(Math.PI / 10);
+
+      let idx = 0;
+      while (idx < sentence.length) {
+        const symbol = sentence[idx];
+
+        if (symbol === "F") {
+          ctx.beginPath();
+          ctx.moveTo(0, 0);
+          ctx.lineTo(0, -branchSize);
+          ctx.stroke();
+          ctx.translate(0, -branchSize);
+          ctx.closePath();
+        } 
+        else if(symbol === "+"){
+          ctx.rotate(-(5 * Math.PI) / 36);
+        }
+        else if(symbol === "-"){
+          ctx.rotate((5 * Math.PI) / 36);
+        }
+        else if (symbol === "[") {
+          ctx.save()
+        } else if (symbol === "]") {
+         ctx.restore();
+        }
+
+        idx++;
+      }
+    }
   }
-  
-  const canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-  
-    const lSystem = new LSystem("0", {
-      "0": "1[0]0",
-      "1": "11"
-    });
-  
-    grammarToString(lSystem, 4);
-  
-    ctx.strokeStyle = "white";
-    ctx.fillStyle = "green";
-  
-    ctx.translate(canvas.width / 2, canvas.height-canvas.height / 6);
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, canvas.height / 6);
-    ctx.stroke();
-    ctx.save();
-  
-    let branchSize = 50;
-    ctx.moveTo(0,0)
-  
-    ctx.save();
-    ctx.rotate(Math.PI / 6);
-    ctx.lineTo(0, -branchSize);
-    ctx.stroke();
-  
-    ctx.moveTo(0,0)
-    ctx.restore();
-    ctx.rotate(-Math.PI / 6);
-    ctx.lineTo(0, -branchSize);
-    ctx.stroke();
-  
-  }
+}
+
+const canvas = document.getElementById("canvas");
+if (canvas.getContext) {
+  const ctx = canvas.getContext("2d");
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "green";
+
+  const lSystem = new LSystem();
+  // const sentence = lSystem.grammarToString(lSystem.grammar.fractalBTree, 11);
+  // lSystem.generateFractal(ctx, 0, sentence, 2.4);
+
+  const sentence = lSystem.grammarToString(lSystem.grammar.fractalPlant, 6);
+  console.log(sentence)
+  lSystem.generateFractal(ctx, 1, sentence, 1.8);
+}
