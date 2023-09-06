cf8b92db57ba53dd3d37a0c5ed623791dccc01ab
Andrew Song <pundrew@Pundrew>
2023-09-06 00:49:00 -0400

    added basic animations - need to find better way to control speed; need to refactor code to achieve separation of concerns and more modular design; also still need to find a way to scale the fractal so that it does not exceed the bounds of the canvas

--- index.js
+++ index.js
@@ -44,26 +44,33 @@ class LSystem {
       ctx.shadowBlur = 14;
       ctx.strokeStyle = "#573E30";
       let idx = 0;
-      while (idx < sentence.length) {
-        const symbol = sentence[idx];
-
-        if (symbol === "1" || symbol === "0") {
-          ctx.beginPath();
-          ctx.moveTo(0, 0);
-          ctx.lineTo(0, -branchSize);
-          if (symbol === "0") ctx.strokeStyle = "green";
-          ctx.stroke();
-          ctx.translate(0, -branchSize);
-          ctx.closePath();
-        } else if (symbol === "[") {
-          ctx.save();
-          ctx.rotate(-Math.PI / 4);
-        } else if (symbol === "]") {
-          ctx.restore();
-          ctx.rotate(Math.PI / 4);
-        }
-
-        idx++;
+      if (idx < sentence.length) {
+        const drawFractal = () => {
+          const symbol = sentence[idx];
+          if (symbol === "1" || symbol === "0") {
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, -branchSize);
+            if (symbol === "0") ctx.strokeStyle = "green";
+            ctx.stroke();
+            ctx.translate(0, -branchSize);
+            ctx.closePath();
+          } else if (symbol === "[") {
+            ctx.save();
+            ctx.rotate(-Math.PI / 4);
+          } else if (symbol === "]") {
+            ctx.restore();
+            ctx.rotate(Math.PI / 4);
+          }
+          idx++;
+          requestAnimationFrame(drawFractal);
+        };
+        requestAnimationFrame(drawFractal);
+        let faster = document.getElementById("faster");
+        faster.addEventListener("click", () => {
+          requestAnimationFrame(drawFractal);
+          console.log("hi")
+        });
       }
     } else if (id === 1) {
       ctx.translate(canvas.width / 2, canvas.height);
@@ -74,28 +81,36 @@ class LSystem {
 
       let idx = 0;
       let len = branchSize;
-      while (idx < sentence.length) {
-        const symbol = sentence[idx];
-
-        if (symbol === "F") {
-          ctx.beginPath();
-          ctx.moveTo(0, 0);
-          ctx.lineTo(0, -len);
-          ctx.stroke();
-          ctx.translate(0, -len);
-          ctx.closePath();
-        } else if (symbol === "+") {
-          ctx.rotate(-(5 * Math.PI) / 36);
-        } else if (symbol === "-") {
-          ctx.rotate((5 * Math.PI) / 36);
-        } else if (symbol === "[") {
-          ctx.strokeStyle = "#573E30";
-          ctx.save();
-          ctx.strokeStyle = "green";
-        } else if (symbol === "]") {
-          ctx.restore();
-        }
-        idx++;
+      if (idx < sentence.length) {
+        const drawFractal = () => {
+          const symbol = sentence[idx];
+          if (symbol === "F") {
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, -len);
+            ctx.stroke();
+            ctx.translate(0, -len);
+            ctx.closePath();
+          } else if (symbol === "+") {
+            ctx.rotate(-(5 * Math.PI) / 36);
+          } else if (symbol === "-") {
+            ctx.rotate((5 * Math.PI) / 36);
+          } else if (symbol === "[") {
+            ctx.strokeStyle = "#573E30";
+            ctx.save();
+            ctx.strokeStyle = "green";
+          } else if (symbol === "]") {
+            ctx.restore();
+          }
+          idx++;
+          requestAnimationFrame(drawFractal);
+        };
+        let faster = document.getElementById("faster");
+        faster.addEventListener("click", () => {
+          requestAnimationFrame(drawFractal);
+          console.log("hi")
+        });
+        requestAnimationFrame(drawFractal);
       }
     }
   }
@@ -112,3 +127,4 @@ if (canvas.getContext) {
   // const sentence = lSystem.grammarToString(lSystem.grammar.fractalPlant, 7);
   // lSystem.generateFractal(ctx, 1, sentence, 1.8);
 }
+
